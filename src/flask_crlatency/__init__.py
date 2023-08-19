b8e950d031762bcfeb61fe8cffb2f1a8e62c41f4
Prince Roshan <princekrroshan01@gmail.com>
2023-08-19 09:25:55 +0530

    Add config param for max_route_latency
    - extension will log the route latency as error
    if time taken is greater then MAX_ROUTE_LATENCY
    - MAX_ROUTE_LATENCY can we configured through app.config

--- src/flask_crlatency/__init__.py
+++ src/flask_crlatency/__init__.py
@@ -1,4 +1,5 @@
-import time 
+import logging
+import time
 
 from flask import request
 
@@ -6,12 +7,14 @@ from flask import request
 class RouteLatencyLogger:
     def __init__(self, app=None):
         self.app = app
+        self.max_route_latency = None
         if app is not None:
             self.init_app(self.app)
 
     def init_app(self, app):
         app.before_request(self.before_request)
         app.after_request(self.after_request)
+        self.max_route_latency = app.config.get('MAX_ROUTE_LATENCY', None)
 
     def before_request(self):
         request._start_time = time.time()
@@ -19,5 +22,11 @@ class RouteLatencyLogger:
     def after_request(self, response):
         if hasattr(request, '_start_time'):
             latency = time.time() - request._start_time
-            self.app.logger.info(f'Latency for route {request.endpoint}: {latency:.6f} seconds')
+            route = request.endpoint
+            print(self.max_route_latency,latency)
+            if self.max_route_latency is not None and latency > self.max_route_latency:
+                logging.warning(f'High latency for route {route}: {latency:.6f} seconds')
+            else:
+                logging.info(f'Latency for route {route}: {latency:.6f} seconds')
+
         return response
